20b1ab83b695abc918b0ee260f7d0515bfa4323a
lloydsheng <i@lloydsheng.com>
2021-01-22 17:01:09 +0800

    Disable auto genreated tests

--- Examples/ExamplesTests/TestableExampleTests.swift
+++ Examples/ExamplesTests/TestableExampleTests.swift
@@ -5,7 +5,7 @@ import ObjectiveC.runtime
 extension UINavigationController {
     func popToRootViewController(animated: Bool, completion: @escaping () -> Void) {
         popToRootViewController(animated: animated)
-
+        
         if animated, let coordinator = transitionCoordinator {
             coordinator.animate(alongsideTransition: nil) { _ in
                 completion()
@@ -17,32 +17,8 @@ extension UINavigationController {
 }
 
 class TestableExampleTests: XCTestCase {
-    private var example: Example!
-
-    override class var defaultTestSuite: XCTestSuite {
-        let newTestSuite = XCTestSuite(forTestCaseClass: TestableExampleTests.self)
-
-        guard let method = class_getInstanceMethod(Self.self, #selector(runExample)) else {
-            fatalError()
-        }
-
-        let existingImpl = method_getImplementation(method)
-
-        for example in Examples.all  {
-            // Add a method for this test, but using the same implementation
-            let selectorName = "test\(example.type)"
-            let testSelector = Selector((selectorName))
-            class_addMethod(Self.self, testSelector, existingImpl, "v@:f")
-
-            let test = TestableExampleTests(selector: testSelector)
-            test.example = example
-            newTestSuite.addTest(test)
-        }
-
-        return newTestSuite
-    }
-
-    @objc private func runExample() {
+    
+func runExample(example: Example) {
         guard let navigationController = UIApplication.shared.windows.first?.rootViewController as? UINavigationController else {
             XCTFail("Root controller is not a UINavigationController")
             return
@@ -62,7 +38,7 @@ class TestableExampleTests: XCTestCase {
         switch result {
         case .completed:
             break
-            
+
         case .timedOut:
             // TODO: check if this is a failure
             print("Example timed out, was this intentional? Call finish() if possible.")
@@ -76,5 +52,11 @@ class TestableExampleTests: XCTestCase {
             popExpectation.fulfill()
         }
         wait(for: [popExpectation], timeout: 5.0)
+    }
+    
+    func testExamples()  {
+        for example in Examples.all  {
+            self.runExample(example: example)
+        }
     }
 }
