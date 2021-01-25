74084bbb6301753926b1d1f97514ddacc575a70f
jmkiley <jordan.kiley@mapbox.com>
2021-01-25 18:58:55 -0500

    Minor updates to annotation docs

--- Mapbox/MapboxMapsAnnotations/AnnotationInteractionDelegate.swift
+++ Mapbox/MapboxMapsAnnotations/AnnotationInteractionDelegate.swift
@@ -1,3 +1,4 @@
+/// A delegate that responds to annotation selection events.
 public protocol AnnotationInteractionDelegate: class {
     func didSelectAnnotation(annotation: Annotation)
     func didDeselectAnnotation(annotation: Annotation)
