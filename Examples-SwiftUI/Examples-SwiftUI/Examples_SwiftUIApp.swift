081e279aba73d094d8a60b6d07ff4a0c6f792fb2
Nadia Barbosa <nadiabarbosa@me.com>
2021-01-28 12:30:50 -0800

    Add SwiftUI target with embedded framework

---
+++ Examples-SwiftUI/Examples-SwiftUI/Examples_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Examples_SwiftUIApp.swift
+//  Examples-SwiftUI
+//
+//  Created by Nadia Barbosa on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Examples_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
