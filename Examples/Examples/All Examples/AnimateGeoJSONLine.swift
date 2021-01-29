848e8bb5bf15a81ad20a54670189ae876f8ee193
Nadia Barbosa <nadiabarbosa@me.com>
2021-01-28 16:03:58 -0800

    Refactor example

--- Examples/Examples/All Examples/AnimateGeoJSONLine.swift
+++ Examples/Examples/All Examples/AnimateGeoJSONLine.swift
@@ -3,187 +3,150 @@ import MapboxMaps
 import Turf
 import CoreLocation
 
-
 @objc(AnimateGeoJSONLine)
 public class AnimateGeoJSONLine: UIViewController, ExampleProtocol {
-    
+
     internal var mapView: MapView!
-    var currentIndex = 1
-    var timer: Timer?
-    var lineLayer: LineLayer!
-    var allCoordinates: [CLLocationCoordinate2D]!
-    var styleManager: StyleManager?
+    internal let sourceIdentifier = "route-source-identifier"
+    internal var routeLineSource: GeoJSONSource!
 
-    public var geoJSONLine = (identifier: "routeLine", source: GeoJSONSource())
-
-    
     override public func viewDidLoad() {
         super.viewDidLoad()
-        
+
         mapView = MapView(with: view.bounds, resourceOptions: resourceOptions())
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         self.view.addSubview(mapView)
-        
+
         let centerCoordinate = CLLocationCoordinate2D(latitude: 45.5076, longitude: -122.6736)
-        
+
         mapView.cameraManager.setCamera(centerCoordinate: centerCoordinate,
                                         zoom: 11.0)
-        
-        allCoordinates = coordinates
-        
+
         // Allows the delegate to receive information about map events.
         mapView.on(.mapLoadingFinished) { [weak self] _ in
-            
+
             guard let self = self else { return }
             self.addLine()
             self.animatePolyline()
-            
+
             // The below line is used for internal testing purposes only.
              self.finish()
         }
     }
-    
+
     func addLine() {
-        let coordinate = coordinates[currentIndex]
-        // Create a GeoJSON data source.
-        geoJSONLine.source.data = .feature(Feature(geometry: Geometry.lineString(LineString([coordinate]))))
-        
+        routeLineSource = GeoJSONSource()
+        routeLineSource.data = .feature(Feature(LineString(allCoordinates)))
+
         var lineLayer = LineLayer(id: "line-layer")
-        lineLayer.filter = Exp(.eq) {
-            "$type"
-            "LineString"
-        }
-        lineLayer.source = geoJSONLine.identifier
+        lineLayer.source = sourceIdentifier
         lineLayer.paint?.lineColor = .constant(ColorRepresentable(color: UIColor.red))
-        
-        let lowZoomWidth = 5
-        let highZoomWidth = 20
-        lineLayer.paint?.lineWidth = .expression(
-            Exp(.interpolate) {
-                Exp(.linear)
-                Exp(.zoom)
-                14
-                lowZoomWidth
-                18
-                highZoomWidth
-            }
-        )
         lineLayer.layout?.lineCap = .round
         lineLayer.layout?.lineJoin = .round
-        
-        // Add the lineLayer to the map.
-        self.mapView.style.addSource(source: geoJSONLine.source, identifier: geoJSONLine.identifier)
-        self.mapView.style.addLayer(layer: lineLayer)
-        
-    }
-    
-    func animatePolyline() {
-        currentIndex = 1
-        
-        // Start a timer that will simulate adding points to our polyline. This could also represent coordinates being added to our polyline from another source, such as a CLLocationManagerDelegate.
-        timer = Timer.scheduledTimer(timeInterval: 0.10, target: self, selector: #selector(tick), userInfo: nil, repeats: true)
-    }
-    
-    @objc func tick() {
-        if currentIndex > allCoordinates.count {
-            timer?.invalidate()
-            timer = nil
-            return
-        }
-        
-        // Create a subarray of locations up to the current index.
-        let coordinates = Array(allCoordinates[0..<currentIndex])
-        
-        // Update our lineLayer with the current locations.
-        updatePolylineWithCoordinates(coordinates: coordinates)
-        
-        currentIndex += 1
-        print(currentIndex)
-    }
-    
-    func updatePolylineWithCoordinates(coordinates: [CLLocationCoordinate2D]) {
-        let mutableCoordinates = coordinates
+        lineLayer.paint?.lineWidth = .constant(6.0)
 
-        // Identify the new coordinate to animate to
-        geoJSONLine.source.data = .feature(Feature(LineString(mutableCoordinates)))
-        let geoJSON = Feature.init(geometry: Geometry.lineString(LineString(mutableCoordinates)))
-        _ = self.mapView.style.updateGeoJSON(for: self.geoJSONLine.identifier,
-                                                                   with: geoJSON)
+        // Add the lineLayer to the map.
+        self.mapView.style.addSource(source: routeLineSource,
+                                     identifier: sourceIdentifier)
+        self.mapView.style.addLayer(layer: lineLayer)
     }
-    
-    var coordinates = [
-        (-122.63748, 45.52214),
-        (-122.64855, 45.52218),
-        (-122.6545, 45.52219),
-        (-122.65497, 45.52196),
-        (-122.65631, 45.52104),
-        (-122.6578, 45.51935),
-        (-122.65867, 45.51848),
-        (-122.65872, 45.51293),
-        (-122.66576, 45.51295),
-        (-122.66745, 45.51252),
-        (-122.66813, 45.51244),
-        (-122.67359, 45.51385),
-        (-122.67415, 45.51406),
-        (-122.67481, 45.51484),
-        (-122.676, 45.51532),
-        (-122.68106, 45.51668),
-        (-122.68503, 45.50934),
-        (-122.68546, 45.50858),
-        (-122.6852, 45.50783),
-        (-122.68424, 45.50714),
-        (-122.68433, 45.50585),
-        (-122.68429, 45.50521),
-        (-122.68456, 45.50445),
-        (-122.68538, 45.50371),
-        (-122.68653, 45.50311),
-        (-122.68731, 45.50292),
-        (-122.68742, 45.50253),
-        (-122.6867, 45.50239),
-        (-122.68545, 45.5026),
-        (-122.68407, 45.50294),
-        (-122.68357, 45.50271),
-        (-122.68236, 45.50055),
-        (-122.68233, 45.49994),
-        (-122.68267, 45.49955),
-        (-122.68257, 45.49919),
-        (-122.68376, 45.49842),
-        (-122.68428, 45.49821),
-        (-122.68573, 45.49798),
-        (-122.68923, 45.49805),
-        (-122.68926, 45.49857),
-        (-122.68814, 45.49911),
-        (-122.68865, 45.49921),
-        (-122.6897, 45.49905),
-        (-122.69346, 45.49917),
-        (-122.69404, 45.49902),
-        (-122.69438, 45.49796),
-        (-122.69504, 45.49697),
-        (-122.69624, 45.49661),
-        (-122.69781, 45.4955),
-        (-122.69803, 45.49517),
-        (-122.69711, 45.49508),
-        (-122.69688, 45.4948),
-        (-122.69744, 45.49368),
-        (-122.69702, 45.49311),
-        (-122.69665, 45.49294),
-        (-122.69788, 45.49212),
-        (-122.69771, 45.49264),
-        (-122.69835, 45.49332),
-        (-122.7007, 45.49334),
-        (-122.70167, 45.49358),
-        (-122.70215, 45.49401),
-        (-122.70229, 45.49439),
-        (-122.70185, 45.49566),
-        (-122.70215, 45.49635),
-        (-122.70346, 45.49674),
-        (-122.70517, 45.49758),
-        (-122.70614, 45.49736),
-        (-122.70663, 45.49736),
-        (-122.70807, 45.49767),
-        (-122.70807, 45.49798),
-        (-122.70717, 45.49798),
-        (-122.70713, 45.4984),
-        (-122.70774, 45.49893)
-    ].map({CLLocationCoordinate2D(latitude: $0.1, longitude: $0.0)})
+
+    func animatePolyline() {
+        var currentIndex = 0
+        var currentCoordinates = [CLLocationCoordinate2D]()
+
+        Timer.scheduledTimer(withTimeInterval: 0.10, repeats: true) { ( timer ) in
+
+            if currentIndex > self.allCoordinates.count {
+                timer.invalidate()
+                return
+            }
+
+            currentIndex += 1
+
+            // Create a subarray of locations up to the current index.
+            currentCoordinates = Array(self.allCoordinates[0..<currentIndex - 1])
+
+            let updatedLine = Feature(LineString(currentCoordinates))
+            self.routeLineSource.data = .feature(updatedLine)
+            _ = self.mapView.style.updateGeoJSON(for: self.sourceIdentifier,
+                                                 with: updatedLine)
+        }
+    }
+
+    let allCoordinates = [
+        CLLocationCoordinate2D(latitude: 45.52214, longitude: -122.63748),
+        CLLocationCoordinate2D(latitude: 45.52218, longitude: -122.64855),
+        CLLocationCoordinate2D(latitude: 45.52219, longitude: -122.6545),
+        CLLocationCoordinate2D(latitude: 45.52196, longitude: -122.65497),
+        CLLocationCoordinate2D(latitude: 45.52104, longitude: -122.65631),
+        CLLocationCoordinate2D(latitude: 45.51935, longitude: -122.6578),
+        CLLocationCoordinate2D(latitude: 45.51848, longitude: -122.65867),
+        CLLocationCoordinate2D(latitude: 45.51293, longitude: -122.65872),
+        CLLocationCoordinate2D(latitude: 45.51295, longitude: -122.66576),
+        CLLocationCoordinate2D(latitude: 45.51252, longitude: -122.66745),
+        CLLocationCoordinate2D(latitude: 45.51244, longitude: -122.66813),
+        CLLocationCoordinate2D(latitude: 45.51385, longitude: -122.67359),
+        CLLocationCoordinate2D(latitude: 45.51406, longitude: -122.67415),
+        CLLocationCoordinate2D(latitude: 45.51484, longitude: -122.67481),
+        CLLocationCoordinate2D(latitude: 45.51532, longitude: -122.676),
+        CLLocationCoordinate2D(latitude: 45.51668, longitude: -122.68106),
+        CLLocationCoordinate2D(latitude: 45.50934, longitude: -122.68503),
+        CLLocationCoordinate2D(latitude: 45.50858, longitude: -122.68546),
+        CLLocationCoordinate2D(latitude: 45.50783, longitude: -122.6852),
+        CLLocationCoordinate2D(latitude: 45.50714, longitude: -122.68424),
+        CLLocationCoordinate2D(latitude: 45.50585, longitude: -122.68433),
+        CLLocationCoordinate2D(latitude: 45.50521, longitude: -122.68429),
+        CLLocationCoordinate2D(latitude: 45.50445, longitude: -122.68456),
+        CLLocationCoordinate2D(latitude: 45.50371, longitude: -122.68538),
+        CLLocationCoordinate2D(latitude: 45.50311, longitude: -122.68653),
+        CLLocationCoordinate2D(latitude: 45.50292, longitude: -122.68731),
+        CLLocationCoordinate2D(latitude: 45.50253, longitude: -122.68742),
+        CLLocationCoordinate2D(latitude: 45.50239, longitude: -122.6867),
+        CLLocationCoordinate2D(latitude: 45.5026, longitude: -122.68545),
+        CLLocationCoordinate2D(latitude: 45.50294, longitude: -122.68407),
+        CLLocationCoordinate2D(latitude: 45.50271, longitude: -122.68357),
+        CLLocationCoordinate2D(latitude: 45.50055, longitude: -122.68236),
+        CLLocationCoordinate2D(latitude: 45.49994, longitude: -122.68233),
+        CLLocationCoordinate2D(latitude: 45.49955, longitude: -122.68267),
+        CLLocationCoordinate2D(latitude: 45.49919, longitude: -122.68257),
+        CLLocationCoordinate2D(latitude: 45.49842, longitude: -122.68376),
+        CLLocationCoordinate2D(latitude: 45.49821, longitude: -122.68428),
+        CLLocationCoordinate2D(latitude: 45.49798, longitude: -122.68573),
+        CLLocationCoordinate2D(latitude: 45.49805, longitude: -122.68923),
+        CLLocationCoordinate2D(latitude: 45.49857, longitude: -122.68926),
+        CLLocationCoordinate2D(latitude: 45.49911, longitude: -122.68814),
+        CLLocationCoordinate2D(latitude: 45.49921, longitude: -122.68865),
+        CLLocationCoordinate2D(latitude: 45.49905, longitude: -122.6897),
+        CLLocationCoordinate2D(latitude: 45.49917, longitude: -122.69346),
+        CLLocationCoordinate2D(latitude: 45.49902, longitude: -122.69404),
+        CLLocationCoordinate2D(latitude: 45.49796, longitude: -122.69438),
+        CLLocationCoordinate2D(latitude: 45.49697, longitude: -122.69504),
+        CLLocationCoordinate2D(latitude: 45.49661, longitude: -122.69624),
+        CLLocationCoordinate2D(latitude: 45.4955, longitude: -122.69781),
+        CLLocationCoordinate2D(latitude: 45.49517, longitude: -122.69803),
+        CLLocationCoordinate2D(latitude: 45.49508, longitude: -122.69711),
+        CLLocationCoordinate2D(latitude: 45.4948, longitude: -122.69688),
+        CLLocationCoordinate2D(latitude: 45.49368, longitude: -122.69744),
+        CLLocationCoordinate2D(latitude: 45.49311, longitude: -122.69702),
+        CLLocationCoordinate2D(latitude: 45.49294, longitude: -122.69665),
+        CLLocationCoordinate2D(latitude: 45.49212, longitude: -122.69788),
+        CLLocationCoordinate2D(latitude: 45.49264, longitude: -122.69771),
+        CLLocationCoordinate2D(latitude: 45.49332, longitude: -122.69835),
+        CLLocationCoordinate2D(latitude: 45.49334, longitude: -122.7007),
+        CLLocationCoordinate2D(latitude: 45.49358, longitude: -122.70167),
+        CLLocationCoordinate2D(latitude: 45.49401, longitude: -122.70215),
+        CLLocationCoordinate2D(latitude: 45.49439, longitude: -122.70229),
+        CLLocationCoordinate2D(latitude: 45.49566, longitude: -122.70185),
+        CLLocationCoordinate2D(latitude: 45.49635, longitude: -122.70215),
+        CLLocationCoordinate2D(latitude: 45.49674, longitude: -122.70346),
+        CLLocationCoordinate2D(latitude: 45.49758, longitude: -122.70517),
+        CLLocationCoordinate2D(latitude: 45.49736, longitude: -122.70614),
+        CLLocationCoordinate2D(latitude: 45.49736, longitude: -122.70663),
+        CLLocationCoordinate2D(latitude: 45.49767, longitude: -122.70807),
+        CLLocationCoordinate2D(latitude: 45.49798, longitude: -122.70807),
+        CLLocationCoordinate2D(latitude: 45.49798, longitude: -122.70717),
+        CLLocationCoordinate2D(latitude: 45.4984, longitude: -122.70713),
+        CLLocationCoordinate2D(latitude: 45.49893, longitude: -122.70774),
+    ]
 }
