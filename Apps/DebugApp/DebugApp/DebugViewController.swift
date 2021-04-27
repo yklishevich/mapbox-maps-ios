91e5d2a60c336c318e810ea8894113b836f31e79
Nishant Karajgikar <nishant.karajgikar@mapbox.com>
2021-04-27 17:33:32 -0400

    Add prototype of background layer animation

--- Apps/DebugApp/DebugApp/DebugViewController.swift
+++ Apps/DebugApp/DebugApp/DebugViewController.swift
@@ -11,67 +11,6 @@ import Turf
 public class DebugViewController: UIViewController {
     
     internal var mapView: MapView!
-    let identifier = "geoJSON-data-source"
-    
-    func addSource() {
-        var geojsonSrc = GeoJSONSource()
-        let feature = Feature(Point(mapView.centerCoordinate))
-        geojsonSrc.data = .feature(feature)
-        let result = mapView.style.addSource(source: geojsonSrc, identifier: identifier)
-        
-        switch result {
-        case .success(_):
-            print("Successfully added source")
-        case .failure(let error):
-            print("Source not added : \(error)")
-        }
-    }
-    
-    func updateSource() {
-    
-        let newGeojsonString =
-        """
-        {
-          "type": "Feature",
-          "properties": {},
-          "geometry": {
-            "type": "LineString",
-            "coordinates": [
-              [
-                139.273681640625,
-                36.01356058518153
-              ],
-              [
-                140.48217773437497,
-                37.64903402157866
-              ]
-            ]
-          }
-        }
-        """
-        
-        let expected = mapView.mapboxMap.__map.setStyleSourcePropertyForSourceId(identifier, property: "data", value: newGeojsonString)
-        if expected.isError() {
-            print("Error in updating source! : \(expected.error)")
-        }
-    }
-    
-    func addLayer() {
-        var lineLayer = LineLayer(id: "my-layer")
-        
-        lineLayer.source = identifier
-        lineLayer.paint?.lineColor = .constant(.init(color: .red))
-        lineLayer.paint?.lineWidth = .constant(3.0)
-        lineLayer.layout?.lineCap = .constant(.round)
-        
-        let result = mapView.style.addLayer(layer: lineLayer)
-        switch result {
-        case .success(_):
-            print("Successfully added layer")
-        case .failure(let error):
-            print("Failed to add layer: \(error)")
-        }
-    }
     
     override public func viewDidLoad() {
         super.viewDidLoad()
@@ -85,16 +24,15 @@ public class DebugViewController: UIViewController {
         let camera = CameraOptions(center: centerCoordinate, zoom: 10)
         mapView.camera.setCamera(to: camera)
         
-        
-        DispatchQueue.main.asyncAfter(deadline: .now()+3) { [weak self] in
-            print("Adding a source")
-            self?.addSource()
+        mapView.on(.mapLoaded) { [weak self](_) in
+            self?.animateBackgroundColorOfLand()
         }
+    }
         
-        DispatchQueue.main.asyncAfter(deadline: .now()+5) { [weak self] in
-            print("Updating a source")
-            self?.updateSource()
-            self?.addLayer()
+    func animateBackgroundColorOfLand() {
+        mapView.style.updateLayer(id: "land", type: BackgroundLayer.self) { (layer) in
+            layer.paint?.backgroundColor = .constant(.init(color: .red))
+            layer.paint?.backgroundColorTransition = .init(duration: 10, delay: 0)
         }
     }
 }
