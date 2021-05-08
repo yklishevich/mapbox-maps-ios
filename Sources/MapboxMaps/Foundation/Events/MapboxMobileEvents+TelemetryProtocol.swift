a39a4df33d6b5b01cdddeaabbed1043d65e8d766
Julian Rex <julian.rex@gmail.com>
2021-05-07 23:32:31 -0400

    turnstile tests

--- Sources/MapboxMaps/Foundation/Events/MapboxMobileEvents+TelemetryProtocol.swift
+++ Sources/MapboxMaps/Foundation/Events/MapboxMobileEvents+TelemetryProtocol.swift
@@ -1,5 +1,6 @@
 import Foundation
 import MapboxMobileEvents
+import os
 
 extension MMEEventsManager: TelemetryProtocol {
     func send(event: String, withAttributes attributes: [String: Any]) {
@@ -11,6 +12,19 @@ extension MMEEventsManager: TelemetryProtocol {
     }
 
     func turnstile() {
-        sendTurnstileEvent()
+
+        if UserDefaults.standard.bool(forKey: "disableEvents") {
+            return
+        }
+
+        if #available(iOS 12.0, *) {
+            let log = OSLog(subsystem: "com.mapbox.maps", category: "events" )
+            let spid = OSSignpostID(log: log)
+            os_signpost(.begin, log: log, name: "turnstile", signpostID: spid)
+            sendTurnstileEvent()
+            os_signpost(.end, log: log, name: "turnstile", signpostID: spid)
+        } else {
+            sendTurnstileEvent()
+        }
     }
 }
